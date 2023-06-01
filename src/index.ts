6bc0a0e4bd763c420c3e24878d7b77c67f6fd388
Sunxiaopeng <xiaopeng.sun@suwell.cn>
2023-06-01 19:09:57 +0800

    fix: 修改构建脚本

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+console.log("Hello World");
+
+export const hello = "Hello World";
+export class Designer {
+  constructor() {
+    console.log("Designer constructor");
+  }
+  public sayHello() {
+    console.log("Hello from Designer");
+    return 0;
+  }
+}
---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+export declare const hello = "Hello World";
+export declare class Designer {
+    constructor();
+    sayHello(): void;
+}
